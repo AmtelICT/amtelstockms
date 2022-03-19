1e0c974896361eda5f49a6e712386eac4693afe6
aamadan <39654460+aamadan@users.noreply.github.com>
2022-03-19 17:40:52 +0300

    19/3/2022

---
+++ client/vue.config.js
@@ -0,0 +1,11 @@
+module.exports={
+    configureWebpack:{
+        devServer:{
+            proxy:{
+                '/api':{
+                    target:'localhost:5000'
+                }
+            }
+        }
+    }
+}
